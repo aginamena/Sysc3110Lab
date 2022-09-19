b76fcf0754eb0508ca41aa6533a70e86fb804c31
Mena Agina <aginamena5@gmail.com>
2022-09-19 06:48:02 -0400

    first commit

---
+++ src/AddressBook.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class AddressBook {
+    List<BuddyInfo> buddyInfo;
+    public AddressBook(){
+        buddyInfo = new ArrayList<>();
+    }
+    public void addBuddy(BuddyInfo buddy){
+        buddyInfo.add(buddy);
+    }
+    public void removeBuddy(BuddyInfo buddy){
+
+    }
+
+    public static void main(String[] a){
+
+    }
+
+}
